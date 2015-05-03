ead824284de8380aff0ed6c84c3689b119368293
@greglgomez <greglgomez@gmail.com>
2015-05-03 02:33:00 +0100

    stream request to file

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var request	= require("request");
 var fs		= require("fs");
 
-request("http://www.sitepoint.com", function(error, response, body) {
-	console.log(body);
-});
-
+request("http://www.sitepoint.com").pipe(fs.createWriteStream("jspro.htm"));
