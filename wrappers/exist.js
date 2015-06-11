9515f19ab484f101e1f6b255fb04379b5246d49b
@greglgomez <greglgomez@gmail.com>
2015-06-11 10:57:12 +0100

    simplified wrapper methods for exist

--- wrappers/exist.js
+++ wrappers/exist.js
@@ -1,64 +1,28 @@
 var config = require('../config');
 var request = require('request');
 
-var qsToday = {};
-var qsInsights = {};
-
-// Exist.io API info - add your own values in config.js
-var options = {
-  url: config.exist.api,
-  headers: config.exist.key
-};
-
-// Standard callback
-// function callback(error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//   	console.log(body);
-//   }
-// }
-
-
+function Exist () {
+	// Exist.io API info - add your own values in config.js
+	this.options = {
+	  	url: config.exist.api,
+	  	headers: config.exist.key
+	};
+}
 
 // API Method Wrappers
-module.exports = {
-
-	// Today
-	// Returns an overview of the user's personal details, and their grouped attributes containing current values. This is analogous to the "Today" tiles in the dashboard.
-
-	today: function () {
-		request(options, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var qsToday = JSON.parse(body);
-				console.log("Retrieved your data successfully:")
-				console.log("Username is " + qsToday.username + ".");
-				console.log(
-				"You have taken " +
-				qsToday.attributes[0].items[0].value +
-				" steps today.");
-			} else {
-				console.log(error);
-			}
-		})
+Exist.prototype = {
+	// Returns an overview of the user's personal details, and their grouped attributes containing current values.
+	today: function (callback) {
+		return request(this.options, callback);
 	},
-
-
-	// Insights
-	// These are the daily snippets of interesting info we find within users' data.
-	// Example URL: https://exist.io/api/1/users/\$self/insights/
-
-	insights: function () {
-		request({
-			url: options.url + "/insights/",
-			headers: options.headers
-		}, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var qsInsights = JSON.parse(body);
-				console.log("Request sent ok:");
-				console.log(body);
-			} else {
-				console.log("Error:");
-				console.log(error);
-			}
-		})
+	// These are the daily snippets of interesting info within users' data.
+	insights: function (callback) {
+		return request('GET', {
+			url: this.options.url + "/insights/",
+			headers: this.options.headers
+		}, callback);
 	}
 };
+
+// Exports
+module.exports = config;
